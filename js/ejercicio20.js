d479955df3765185f6e122f6d5991e63ab609f12
JuanAr02 <arzejuanignacio06@gmail.com>
2026-05-03 19:44:51 -0300

    agrego ejercicio 20

---
+++ js/ejercicio20.js
@@ -0,0 +1,13 @@
+document.querySelector("#btnMostrar").addEventListener("click", function() {
+
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const libreta = document.querySelector("#libreta").value;
+
+    alert(
+        "Los datos ingresados son:\n" +
+        "Nombre: " + nombre + "\n" +
+        "Apellido: " + apellido + "\n" +
+        "Libreta Universitaria: " + libreta
+    );
+});
